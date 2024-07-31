59fea4caacb4e8698e0052ce06dba1f0f94a1e0f
TheSamVil <svillav4@eafit.edu.co>
2024-07-31 10:51:09 -0500

    changes in the home function

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Samuel Villa'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
